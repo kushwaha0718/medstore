a0c5e9bc331e93189fc2959133b06435514956fd
Ayush Kushwaha <kushwaha.coding@gmail.com>
2025-12-22 21:22:48 +0530

    Initial commit

---
+++ src/main/java/com/nitish/medstore/MedstoreApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.medstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitish/medstore/MedstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.medstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
